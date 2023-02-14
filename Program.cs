fe41301b52e7ec30a7cf2cf4a0aa91fb689fcf07
Valdiney Daniel Garcia <valdineysc@gmail.com>
2023-02-13 22:55:55 -0300

    Primeira versão sem1_exercicio1

---
+++ Program.cs
@@ -0,0 +1,12 @@
+﻿string nomeUsuario, sobrenomeUsuario, corFavorita;
+Console.WriteLine("Seja Bem vindo! Favor informar seu nome:");
+nomeUsuario = Console.ReadLine();
+Console.WriteLine("Informe seu sobrenome:");
+sobrenomeUsuario = Console.ReadLine();
+Console.WriteLine("Qual a sua cor favorita?");
+corFavorita = Console.ReadLine();
+int idadeUsuario;
+Console.WriteLine("Qual a sua idade?");
+idadeUsuario = int.Parse(Console.ReadLine());
+Console.WriteLine($"Obrigado por responder as perguntas {nomeUsuario} {sobrenomeUsuario}. Você tem {idadeUsuario} anos e sua cor preferida é {corFavorita}.");
+
